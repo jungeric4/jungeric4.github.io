a5caa592272324144a51aee373967e962214e07c
jungeric4 <>
2019-04-02 00:27:44 -0400

    Fixed Navbar UI

--- init.js
+++ init.js
@@ -1,14 +1,13 @@
 
 navbar = String.raw`
-<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </nav>`;
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <a class="nav-item nav-link" href="#" style="color:#FFFFFF">Navbar</a>
+                  <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home</a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                  </div>
+                </div>
+              </nav>`;
 
 function loadNavbar() {
     $('body').prepend(navbar);
