442bf987c400f35f63adc71e933e0eb6a1fbb467
jungeric4 <>
2019-04-02 02:10:34 -0400

    Abstracted Sidebar into one thing

--- init.js
+++ init.js
@@ -9,10 +9,47 @@ navbar = String.raw`
                 </div>
               </nav>`;
 
+sidebar = String.raw`
+<div class="container-fluid">
+              <div class="row">
+                  <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                      <div class="sidebar-sticky">
+                          <ul class="nav flex-column">
+                              <li class="nav-item">
+                                  <a class="nav-link" href="#Overview Text">
+                                      Overview
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="#Design Text">
+                                      Design
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="#Validation Text">
+                                      Validation
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link" href="#Report">
+                                      Report
+                                  </a>
+                              </li>
+                          </ul>
+                          </ul>
+                      </div>
+                  </nav>`;
+
 function loadNavbar() {
-    $('body').prepend(navbar);
+  $('body').prepend(navbar);
+}
+
+function loadSidebar(){
+  $('body').prepend(sidebar);
 }
 
 function initPage() {
-    loadNavbar()
+  loadSidebar();
+  loadNavbar();
 }
+
